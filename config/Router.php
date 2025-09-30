8ea265d7dee77fe11ce813d772630430b93ad5e9
mohamedzekalmi <zmohamedilies@gmail.com>
2025-09-30 12:30:23 +0200

    woinwoin

--- config/Router.php
+++ config/Router.php
@@ -30,7 +30,7 @@ class Router
     {
         $this->routes[$pattern] = [
             'controller' => $controllerName,
-            'method' => $method
+            'method' => $method                                                                                     
         ];
     }
 
