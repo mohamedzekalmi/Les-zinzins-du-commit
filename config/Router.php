cd75e0a1976a43743e3ba97a7bb9fe538fd09e4a
mohamedzekalmi <zmohamedilies@gmail.com>
2025-10-01 10:07:39 +0200

    yaaa

--- config/Router.php
+++ config/Router.php
@@ -1,6 +1,6 @@
 <?php
 // Le namespace "config" permet de ranger la classe Router dans un espace logique et d'éviter les conflits de noms.
-namespace config;
+namespace Config;
 
 // On importe le contrôleur d'erreurs pour afficher une page 404
 use App\Controllers\ErrorController;
@@ -26,11 +26,11 @@ class Router
      * @param string $controllerClass Le nom du contrôleur (ex: "LoginController")
      * @param string $method La méthode du contrôleur à exécuter (ex: "index")
      */
-    public function addRoute(string $pattern, string $controllerName, string $method)
+    public function addRoute(string $nameURI, string $controllerName, string $method)
     {
-        $this->routes[$pattern] = [
+        $this->routes[$nameURI] = [
             'controller' => $controllerName,
-            'method' => $method                                                                                     
+            'method' => $method
         ];
     }
 
